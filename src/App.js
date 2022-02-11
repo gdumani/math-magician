bf55f0b1645120976c0e05351bf5ca56921a9afa
gdumani <gdumani@gmail.com>
2022-02-11 14:33:56 -0600

    refactor App.js into function

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import React from 'react';
 import Calculator from './components/calculator';
 import './App.css';
 
-class App extends React.PureComponent {
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Calculator />
+  </div>
+);
 
 export default App;
