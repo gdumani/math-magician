fbaffae9b319c62d54b2c5f649594f0376c84b78
gdumani <gdumani@gmail.com>
2022-02-11 16:06:38 -0600

    refactor calculator.js to use hooks

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,47 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const Calculator = () => {
+  const [state, setstate] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  handleClick(e) {
-    this.setState((state) => calculate(state, e.target.textContent));
-  }
+  const handleClick = (e) => setstate(calculate(state, e.target.textContent));
+  const { total, next, operation } = state;
+  const disp = `${total || ''}${operation || ''}${next || ''}`;
+  return (
+    <div className="calc">
+      <span>{disp}</span>
+      <button onClick={handleClick} type="button">AC</button>
+      <button onClick={handleClick} type="button">+/-</button>
+      <button onClick={handleClick} type="button">%</button>
+      <button onClick={handleClick} type="button" className="colored">÷</button>
+      <button onClick={handleClick} type="button">7</button>
+      <button onClick={handleClick} type="button">8</button>
+      <button onClick={handleClick} type="button">9</button>
+      <button onClick={handleClick} type="button" className="colored">x</button>
+      <button onClick={handleClick} type="button">4</button>
+      <button onClick={handleClick} type="button">5</button>
+      <button onClick={handleClick} type="button">6</button>
+      <button onClick={handleClick} type="button" className="colored">-</button>
+      <button onClick={handleClick} type="button">1</button>
+      <button onClick={handleClick} type="button">2</button>
+      <button onClick={handleClick} type="button">3</button>
+      <button onClick={handleClick} type="button" className="colored">+</button>
+      <button onClick={handleClick} type="button" className="double">0</button>
+      <button onClick={handleClick} type="button">.</button>
+      <button onClick={handleClick} type="button" className="colored">=</button>
+    </div>
+  );
+};
 
-  render() {
-    const { total, next, operation } = this.state;
-    const disp = `${total || ''}${operation || ''}${next || ''}`;
-    return (
-      <div className="calc">
-        <span>{disp}</span>
-        <button onClick={this.handleClick} type="button">AC</button>
-        <button onClick={this.handleClick} type="button">+/-</button>
-        <button onClick={this.handleClick} type="button">%</button>
-        <button onClick={this.handleClick} type="button" className="colored">÷</button>
-        <button onClick={this.handleClick} type="button">7</button>
-        <button onClick={this.handleClick} type="button">8</button>
-        <button onClick={this.handleClick} type="button">9</button>
-        <button onClick={this.handleClick} type="button" className="colored">x</button>
-        <button onClick={this.handleClick} type="button">4</button>
-        <button onClick={this.handleClick} type="button">5</button>
-        <button onClick={this.handleClick} type="button">6</button>
-        <button onClick={this.handleClick} type="button" className="colored">-</button>
-        <button onClick={this.handleClick} type="button">1</button>
-        <button onClick={this.handleClick} type="button">2</button>
-        <button onClick={this.handleClick} type="button">3</button>
-        <button onClick={this.handleClick} type="button" className="colored">+</button>
-        <button onClick={this.handleClick} type="button" className="double">0</button>
-        <button onClick={this.handleClick} type="button">.</button>
-        <button onClick={this.handleClick} type="button" className="colored">=</button>
-      </div>
-    );
-  }
-}
+export default Calculator;
