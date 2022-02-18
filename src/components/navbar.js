649c6b103e2f5fc96e66b76307874405da0aaa90
gdumani <gdumani@gmail.com>
2022-02-18 16:56:26 -0600

    fix navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,15 +3,18 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => (
   <div>
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-    <li>
-      <Link to="/calc">Calculator</Link>
-    </li>
-    <li>
-      <Link to="/quote">Quotes</Link>
-    </li>
+    <h1>Math Magician</h1>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/calc">Calculator</Link>
+      </li>
+      <li>
+        <Link to="/quote">Quotes</Link>
+      </li>
+    </ul>
   </div>
 );
 export default NavBar;
